3ce8e00f8e40be5c322ab28028f8edc73bc9f45b
lprussak <larissaprussak@gmail.com>
2020-03-29 22:21:49 -0300

    remover node modules

--- Backend/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const configuration = require('../../knexfile') //importa conf. do knexfile
-
-const connection = knex(configuration.development)//define ambiente para dev
-
-module.exports = connection;
